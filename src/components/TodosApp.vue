503530bafbdd532a108e38a452e263c6419779e2
Dylan Trepos <dylan.trepos@gmail.com>
2022-04-02 15:58:12 +0200

    API to add a new task with a TodosApp Vue component

---
+++ src/components/TodosApp.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="todoapp">
+        <header class="header">
+            <h1>Todos</h1>
+            <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo" @keyup.enter="addTodo">
+        </header>
+        <div class="main">
+            <ul class="todo-list">
+                <li class="todo" v-for="(todo, index) in todos" :key="index" :class="{completed: todo.completed}">
+                    <div class="view">
+                        <input type="checkbox" v-model="todo.completed" class="toggle">
+                        <label for="">{{ todo.name }}</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todos: [{
+                name: 'Tache de test',
+                completed: false
+            }],
+            newTodo: ''
+        }
+    },
+
+    methods: {
+        addTodo() {
+            this.todos.push({
+                name: this.newTodo,
+                completed: false
+            });
+            this.newTodo = '';
+        },
+    }
+
+}
+</script>
+
+<style src="./todos.css">
+
+</style>
